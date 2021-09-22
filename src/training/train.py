4b2b48e7a03dc6094253525063ab39f89689d878
kkulesz <kulesza.konrad@protonmail.com>
2021-09-22 18:19:56 +0200

    train skeleton of skeleton

---
+++ src/training/train.py
@@ -0,0 +1,18 @@
+
+
+class CycleGanTraining:
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def _prepare_dataloader(self):
+        pass
+
+    def _validate(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
--- test/dummy_test.py
+++ test/dummy_test.py
@@ -1,3 +1,4 @@
 if __name__ == '__main__':
+    print("Nothin here ;<")
     # giving up setting workflow for now because it will cost quite a lot of time because of some modules and paths
     exit(0)
