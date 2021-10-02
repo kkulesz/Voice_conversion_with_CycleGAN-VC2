3d0d9c6fda471d64dd1e63f439e4781bbfd92621
kkulesz <kulesza.konrad@protonmail.com>
2021-10-02 22:22:35 +0200

    give up once again

--- test/modules_test.py
+++ test/modules_test.py
@@ -1,16 +1,22 @@
-import pytest
-from src.model.generator import Generator
+import unittest
 
-def test_discriminator_dimensionality():
-    print("I'm")
-    assert True
+import src.model.generator
+import src.model.discriminator
 
 
-def test_generator_dimensionality():
-    print("finally")
-    assert True
+class ModulesTests(unittest.TestCase):
+    def test_discriminator_dimensionality(self):
+        print("I'm")
+        self.assertEqual(True, True)
+
+    def test_generator_dimensionality(self):
+        print("finally")
+        self.assertEqual(True, True)
+
+    def test_gen_output_as_disc_input(self):
+        print("running!")
+        self.assertEqual(True, True)
 
 
-def test_gen_output_as_disc_input():
-    print("running!")
-    assert True
+if __name__ == '__main__':
+    unittest.main()
