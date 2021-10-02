229f01bd1f6cbe41cc9913b98ae2b30a95456606
kkulesz <kulesza.konrad@protonmail.com>
2021-10-02 21:18:11 +0200

    get rid of file paths

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 
 setup(name='crazy setup',
       version='0.0.1',
-      packages=['src', 'src/model', 'src/model/generator', 'src/model/discriminator', 'test']
+      packages=['src', 'src/model', 'test']
       )
