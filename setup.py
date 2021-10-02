c875af5c7d3b5b3e7ba0b701ce60440867cdc57b
kkulesz <kulesza.konrad@protonmail.com>
2021-10-02 21:03:36 +0200

    mofidy setup.py and workflow

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
-# from setuptools import find_packages
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='crazy setup',
       version='0.0.1',
-      packages=['src', 'test']
+      packages=find_packages()
       )
