ab06f16cf27faa6c94af0e97a6c9aa00781d743b
kkulesz <kulesza.konrad@protonmail.com>
2021-11-29 18:11:58 +0100

    minor changes in plotting script

--- scripts/plot_losses.py
+++ scripts/plot_losses.py
@@ -7,16 +7,18 @@ from consts import Consts
 
 if __name__ == '__main__':
     directory_path = Consts.models_storage_directory_path
+    # directory_path = 'E:\\STUDIA\inzynierka\\2_moje_przygotowania\\4.trenowanie\\2.SM1-TM1\\models_storage'
     discriminators_losses_file_path = os.path.join(directory_path, Consts.discriminator_loss_storage_file)
     generators_losses_file_path = os.path.join(directory_path, Consts.generator_loss_storage_file)
 
-    plot_after = int(0 * 1e+3)
+    plot_after = int(5 * 1e+3)
+    plot_before = int(6 * 1e+3)
 
     disc_df = pd.read_csv(discriminators_losses_file_path)
     gen_df = pd.read_csv(generators_losses_file_path)
 
-    wanted_disc_df = disc_df.iloc[plot_after:]
-    wanted_gen_df = gen_df.iloc[plot_after:]
+    wanted_disc_df = disc_df.iloc[plot_after:plot_before]
+    wanted_gen_df = gen_df.iloc[plot_after:plot_before]
 
     wanted_disc_df.plot(x=0, y=1, kind='scatter', title="Discriminator", ylabel='Loss', xlabel="Iteration")
     wanted_gen_df.plot(x=0, y=1, kind='scatter', title='Generator', ylabel='Loss', xlabel="Iteration")
